b12d0de104ebf24736153546113d4d91df762532
eric-lim <eric.lim@xero.com>
2020-04-04 22:59:40 +1300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.scss';
 import Dashboard from './containers/Dashboard';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
 
-const App = () => {
-  return <Dashboard />;
+const App: React.FC = () => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
