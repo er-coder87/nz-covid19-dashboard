cf4119f9ce60f436d6ae4e15d0037a00098c4bb0
eric-lim <eric.lim@xero.com>
2020-04-05 00:43:01 +1300

    add build

---
+++ build/precache-manifest.80714376e1c409e29279f671432417c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da50f77b2bcc50b6d75ffd7e8af3ceeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d04c0689d6d3f5e17fdd",
+    "url": "/static/css/main.1180f1c5.chunk.css"
+  },
+  {
+    "revision": "aa06d41fb6ecbffd4703",
+    "url": "/static/js/2.3af116ae.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.3af116ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04c0689d6d3f5e17fdd",
+    "url": "/static/js/main.96931bc1.chunk.js"
+  },
+  {
+    "revision": "67c7c0a93a09a93c8c0b",
+    "url": "/static/js/runtime-main.925cbcc3.js"
+  }
+]);
