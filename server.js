573a85682301f201b25520f04a0dbe652631cfcf
eric-lim <eric.lim@xero.com>
2020-04-10 16:13:43 +1200

    compare files to correctly identify if downloaded file is different to the current file

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ app.listen(port, () => {
   console.log(`server has started on port ${port}`);
 });
 
-let currentXlsxName = 'covid-casedetails-9april2020.xlsx';
+const current_xlsx = 'current.xlsx';
 // assume lastUpdatedDate is yesterday
 const appStartDate = new Date();
 let lastUpdatedDate = appStartDate.setDate(appStartDate.getDate() - 1);
 
 app.get('/api/data', async (req, res) => {
   try {
-    const workbook = XLSX.readFile(currentXlsxName);
+    const workbook = XLSX.readFile(current_xlsx);
     const confirmedCasesSheet = workbook.Sheets[workbook.SheetNames[0]];
     const probableCasesSheet = workbook.Sheets[workbook.SheetNames[1]];
     const confirmedCases = XLSX.utils.sheet_to_json(confirmedCasesSheet, { range: 3, raw: false });
@@ -61,36 +61,43 @@ const months = [
   'november',
   'december',
 ];
-// schedule tasks to be run on the server
+
 const cron = require('node-cron');
-// schedule tasks to be run on the server
 cron.schedule('0 */1 * * *', function () {
   console.log('Running Cron Job');
   const today = new Date();
   const formattedToday = today.getDate() + months[today.getMonth()] + today.getFullYear();
-  const todayXlsxName = `covid-casedetails-${formattedToday}.xlsx`;
-  if (!fs.existsSync(todayXlsxName)) {
-    console.log('file does not exist');
-
-    console.log(formattedToday);
-    download(
-      `https://www.health.govt.nz/system/files/documents/pages/covid-casedetails-${formattedToday}.xlsx`,
-      todayXlsxName,
-    );
-    lastUpdatedDate = today;
-
-    currentXlsxName = todayXlsxName;
-    console.log(todayXlsxName, 'finished downloading');
-  } else {
-    console.log(`File ${currentXlsxName} already exists`);
-  }
+  const downloaded_xlsx = 'downloaded.xlsx';
+  download(
+    `https://www.health.govt.nz/system/files/documents/pages/covid-casedetails-${formattedToday}.xlsx`,
+    downloaded_xlsx,
+    compareAndSave,
+  );
 });
+
+const compareAndSave = () => {
+  const downloaded_xlsx = 'downloaded.xlsx';
+  const currentFileBuffer = fs.readFileSync(current_xlsx);
+  const downloadedFileBuffer = fs.readFileSync(downloaded_xlsx);
+
+  if (currentFileBuffer.equals(downloadedFileBuffer)) {
+    console.log(`File already exists`);
+  } else {
+    console.log('New file detected');
+    fs.renameSync(downloaded_xlsx, current_xlsx);
+
+    const today = new Date();
+    lastUpdatedDate = today;
+    currentXlsxName = current_xlsx;
+  }
+};
 
 const download = (url, dest, cb) => {
   var file = fs.createWriteStream(dest);
   https.get(url, function (response) {
     response.pipe(file);
     file.on('finish', function () {
+      console.log('finish downloading');
       file.close(cb);
     });
   });
