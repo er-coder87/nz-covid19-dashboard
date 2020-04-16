27b77446c08396794105e6782904b113c32c2424
eric-lim <eric.lim@xero.com>
2020-04-16 17:24:52 +1200

    hardcode link

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ const formattedToday = today.getDate() + months[today.getMonth()]; // + today.ge
 //https://www.health.govt.nz/system/files/documents/pages/covid-case-list-13april2020.xlsx
 //`https://www.health.govt.nz/system/files/documents/pages/covid-case-list-${formattedToday}.xlsx`
 //https://www.health.govt.nz/system/files/documents/pages/covid-caselist-14april.xlsx
-const downloadLink = `https://www.health.govt.nz/system/files/documents/pages/covid-case_list-${formattedToday}.xlsx`;
+//https://www.health.govt.nz/system/files/documents/pages/covid-case_list-16-april.xlsx
+const downloadLink = `https://www.health.govt.nz/system/files/documents/pages/covid-case_list-16-april.xlsx`;
 
 let confirmedCases;
 let probableCases;
